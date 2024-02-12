c1f2a76ed33ab2e5a14787b6fde020c20fc3928c
choonghyun <a0107163@naver.com>
2024-02-12 23:23:27 +0900

    question 10171

---
+++ q_10171/Main.java
@@ -0,0 +1,10 @@
+package q_10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
